f97aae4e6aec6c9e9cd0618d031cbe8c14269e54
Vang-er <mohammed.saber.abdelazim@gmail.com>
2026-08-04 09:05:30 +0300

    init the project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react"; 
+export default function Myapp(){
+    return (<>
+        <nav className="bg-green-600 shadow-md text-white"><div className="max-w-7xl mx-auto h-16 px-6 items-center justify-between"><div className="font-bold cursor-pointer text-2xl">Donate For All</div></div></nav>
+        <div>hi2</div>
+    </>)
+} 
