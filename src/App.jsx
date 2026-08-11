b9035b92f9465780a49ac747d674c7c07408f7c4
mernanaguib16-glitch <mernanaguib16@gmail.com>
2026-08-12 01:08:03 +0300

    connect signin and signup pages navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
- import { Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+
+function SignInWrapper() {
+  const navigate = useNavigate();
+  return <SignIn onNavigateToSignUp={() => navigate("/signup")} />;
+}
+
+function SignUpWrapper() {
+  const navigate = useNavigate();
+  return <SignUp onNavigateToSignIn={() => navigate("/login")} />;
+}
 
 export default function MyApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<SignInWrapper />} />
+      <Route path="/signup" element={<SignUpWrapper />} />
     </Routes>
   );
 }
